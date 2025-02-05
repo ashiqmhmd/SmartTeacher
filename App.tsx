78138676a2583981e9b61d5a5be028fc5b4d952e
shaniOO3 <official.solo003@gmail.com>
2025-02-05 23:29:54 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -121,6 +121,12 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import FeesScreen from './src/screens/feesScreen';
+import AssignmentScreen from './src/screens/AssignmentScreen';
+import NotesScreen from './src/screens/NotesScreen';
+import ChatsScreen from './src/screens/ChatsScreen';
 
 const Stack = createStackNavigator();
 
@@ -131,6 +137,12 @@ const App = () => {
         initialRouteName="Home"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Fees" component={FeesScreen} />
+        <Stack.Screen name="Assignment" component={AssignmentScreen} />
+        <Stack.Screen name="Notes" component={NotesScreen} />
+        <Stack.Screen name="Chats" component={ChatsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
