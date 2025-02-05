64cd9341f75f9d362d751637f62d4dc38df61d8b
shaniOO3 <official.solo003@gmail.com>
2025-02-05 23:30:02 +0530

    Create ChatsScreen.tsx

---
+++ src/screens/ChatsScreen.tsx
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BottomNavigation from '../components/BottomNavBar';
+
+const ChatsScreen = ({navigation}) => {
+  return (
+    <View style={styles.chatsScreen}>
+      <Text>ChatsScreen</Text>
+      <Text>ChatsScreen</Text>
+      <BottomNavigation navigation={navigation} />
+    </View>
+  );
+};
+
+export default ChatsScreen;
+
+const styles = StyleSheet.create({
+  chatsScreen: {flex: 1},
+});
