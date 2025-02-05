2b847d542e07219f8a526b8ab500b26ed7112b23
shaniOO3 <official.solo003@gmail.com>
2025-02-05 23:30:04 +0530

    Create FeesScreen.tsx

---
+++ src/screens/FeesScreen.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BottomNavigation from '../components/BottomNavBar';
+
+const FeesScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>FeesScreen</Text>
+      <Text>FeesScreen</Text>
+      <Text>FeesScrdsvxveen</Text>
+      {/* <BottomNavigation navigation={navigation} /> */}
+    </View>
+  );
+};
+
+export default FeesScreen;
+
+const styles = StyleSheet.create({
+  //   feesScreen: {flex: 1},
+});
