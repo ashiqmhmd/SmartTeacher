c0d21f852a4b4a864b160734c5d12998582648d3
shaniOO3 <official.solo003@gmail.com>
2025-02-05 23:30:11 +0530

    Create NotesScreen.tsx

---
+++ src/screens/NotesScreen.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BottomNavigation from '../components/BottomNavBar';
+
+const NotesScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>NotesScreen</Text>
+      <BottomNavigation navigation={navigation} />
+    </View>
+  );
+};
+
+export default NotesScreen;
+
+const styles = StyleSheet.create({});
