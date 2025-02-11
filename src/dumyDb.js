0784e6102f7f7c948170fcfc8735f1da5d937afb
shaniOO3 <official.solo003@gmail.com>
2025-02-11 23:41:25 +0530

    Update dumyDb.js

--- src/dumyDb.js
+++ src/dumyDb.js
@@ -227,3 +227,84 @@ export const batches = [
     studentCount: 28,
   },
 ];
+
+export const notes = [
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note .',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: '21165381-a163-4736-9d92-07bb7e5d6ed5',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note.',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: 'd58fe232-c6bd-4c60-b7b6-c0e24c7f8231',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note.',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: 'd5f2196c-2667-496b-a10e-98fe3c368458',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note.',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: '5bacffc3-2d5d-48cf-8cfe-4b6b4a986e11',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note.',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: '4bbd6049-aebf-4cdc-b8eb-f631851072e8',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  {
+    content:
+      'This is the content of the new note, providing detailed information.',
+    Title: 'This is a new note.',
+    studentId: '1e4567-e89b-12d3-a456-42661417',
+    listUrls: [
+      'http://example.com/resource1.pdf',
+      'http://example.com/resource2.pdf',
+    ],
+    publishDate: '2024-01-01T00:00:00.000Z',
+    id: '2aff9a51-ce62-4266-8189-63c861f85f32',
+    batchId: '123e4567-e89b-12d3-a456-426614174000',
+  },
+];
