93e3f3c6b3db9589614f7d0d3ef6d82c832a4092
shaniOO3 <official.solo003@gmail.com>
2025-02-12 23:40:19 +0530

    Update store.tsx

--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,22 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
-import authReducer from "../utils/authslice";
+import {configureStore} from '@reduxjs/toolkit';
+import {persistStore, persistReducer} from 'redux-persist';
+import authReducer from '../utils/authslice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage, // Use AsyncStorage for persistence
-  };
+  key: 'root',
+  storage: AsyncStorage,
+};
 
-  const persistedReducer = persistReducer(persistConfig, authReducer);
-
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
-    reducer:{
-        auth: persistedReducer
-    }
-
-})
+  reducer: {
+    auth: persistedReducer,
+  },
+});
 export const persistor = persistStore(store);
 export default store;
-console.log("store print")
-console.log(store)
+console.log('store print');
+console.log(store);
