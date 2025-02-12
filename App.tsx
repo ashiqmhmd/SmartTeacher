3826a68053f0df5659dd8afc93a053f58dff8289
shaniOO3 <official.solo003@gmail.com>
2025-02-12 23:39:57 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -118,20 +118,17 @@
 // export default App;
 
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import Rootnavigator from './src/utils/Rootnavigator';
-import { Provider } from 'react-redux';
-import store, { persistor } from './src/utils/store';
-import { PersistGate } from 'redux-persist/integration/react';
-
-
-
+import {Provider} from 'react-redux';
+import store, {persistor} from './src/utils/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer style={{ backgroundColor: 'transparent' }}>
+        <NavigationContainer style={{backgroundColor: 'transparent'}}>
           <Rootnavigator />
         </NavigationContainer>
       </PersistGate>
