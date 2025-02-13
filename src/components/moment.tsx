ce1ca9190a4e043d242c3f80a5a46d62f581ac84
shaniOO3 <official.solo003@gmail.com>
2025-02-13 21:41:40 +0530

    Update moment.tsx

--- src/components/moment.tsx
+++ src/components/moment.tsx
@@ -1,8 +1,8 @@
 import moment from 'moment';
-const dateconvert = (date) => {
-    const isoDate = date;
-    const formattedDate = moment(isoDate).format('D,M,YYYY'); // Example: "January 15, 2024"
-    return formattedDate;
-}  
+const dateconvert = date => {
+  const isoDate = date;
+  const formattedDate = moment(isoDate).format('DD/MM/YYYY');
+  return formattedDate;
+};
 
-export default dateconvert
+export default dateconvert;
