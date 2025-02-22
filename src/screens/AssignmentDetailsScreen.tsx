b16aa2f2b3a1c582d15c1c49198bc6c977941a78
shaniOO3 <official.solo003@gmail.com>
2025-02-22 19:11:02 +0530

    Create AssignmentDetailsScreen.tsx

---
+++ src/screens/AssignmentDetailsScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AssignmentDetailsScreen = () => {
+  return (
+    <View>
+      <Text>AssignmentDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default AssignmentDetailsScreen;
+
+const styles = StyleSheet.create({});
