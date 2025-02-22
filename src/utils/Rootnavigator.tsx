5badcd916e929e02130ebd2214d024bcbdee5502
shaniOO3 <official.solo003@gmail.com>
2025-02-22 19:11:57 +0530

    Update Rootnavigator.tsx

--- src/utils/Rootnavigator.tsx
+++ src/utils/Rootnavigator.tsx
@@ -15,6 +15,9 @@ import NoteCreateScreen from '../screens/NoteCreateScreen';
 import BatchCreation from '../screens/BatchCreation';
 import StudentCreation from '../screens/StudentCreation';
 import StudentDetailsScreen from '../screens/StudentDetailsScreen';
+import FeePaymentDetailsScreen from '../screens/FeePaymentDetailsScreen';
+import AssignmentDetailsScreen from '../screens/AssignmentDetailsScreen';
+import NoteDetailsScreen from '../screens/NoteDetailsScreen';
 // import ConversationScreen from '../screens/ConversationScreen';
 
 type Props = {};
@@ -39,10 +42,16 @@ const Rootnavigator = () => {
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="Update_Profile" component={UpdateProfileScreen} />
       <Stack.Screen name="Fees" component={FeesScreen} />
+      <Stack.Screen name="Fees_Detail" component={FeePaymentDetailsScreen} />
       <Stack.Screen name="Assignment" component={AssignmentsScreen} />
       <Stack.Screen name="Assignment_Creation" component={CreateAssignment} />
+      <Stack.Screen
+        name="Assignment_Detail"
+        component={AssignmentDetailsScreen}
+      />
       <Stack.Screen name="Notes" component={NotesScreen} />
       <Stack.Screen name="Note_Create" component={NoteCreateScreen} />
+      <Stack.Screen name="Note_Detail" component={NoteDetailsScreen} />
       <Stack.Screen name="Chats" component={ChatsScreen} />
       {/* <Stack.Screen name="Chat" component={ConversationScreen} /> */}
       <Stack.Screen name="Student_Create" component={StudentCreation} />
