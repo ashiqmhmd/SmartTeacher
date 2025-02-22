ba0b9c9a23b773fa79f20e889e3da91f7f135baa
shaniOO3 <official.solo003@gmail.com>
2025-02-22 19:12:02 +0530

    Create NoteDetailsScreen.tsx

---
+++ src/screens/NoteDetailsScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const NoteDetailsScreen = () => {
+  return (
+    <View>
+      <Text>NoteDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default NoteDetailsScreen;
+
+const styles = StyleSheet.create({});
