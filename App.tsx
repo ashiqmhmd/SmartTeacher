b5f04af815bb72431c2db6a222620583c193c9ab
shaniOO3 <official.solo003@gmail.com>
2025-05-01 12:04:40 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,14 +4,18 @@ import Rootnavigator from './src/utils/Rootnavigator';
 import {Provider} from 'react-redux';
 import store, {persistor} from './src/utils/store';
 import {PersistGate} from 'redux-persist/integration/react';
-import { linking } from './src/utils/Linking';
+import {linking} from './src/utils/Linking';
+import Toast from 'react-native-toast-message';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer style={{backgroundColor: 'transparent'}} linking={linking}>
+        <NavigationContainer
+          style={{backgroundColor: 'transparent'}}
+          linking={linking}>
           <Rootnavigator />
+          <Toast />
         </NavigationContainer>
       </PersistGate>
     </Provider>
