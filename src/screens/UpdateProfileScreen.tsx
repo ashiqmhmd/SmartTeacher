3aa6b70b0bb07b9c329cbdc0bf2e9a0864648054
ashiqmhmd <69426406+ashiqmhmd@users.noreply.github.com>
2025-08-28 11:26:43 +0530

    toast setted for profile image

--- src/screens/UpdateProfileScreen.tsx
+++ src/screens/UpdateProfileScreen.tsx
@@ -127,6 +127,12 @@ const UpdateProfileScreen = ({navigation, route}) => {
 
       if (result.success) {
         setProfileImage(result.uri);
+         Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'Profile photo uploaded',
+          visibilityTime: 3000,
+        });
 
         setProfileData(prev => ({
           ...prev,
@@ -145,6 +151,12 @@ const UpdateProfileScreen = ({navigation, route}) => {
           ...prev,
           profilePicUrl: 'Failed to upload profile image. Please try again.',
         }));
+         Toast.show({
+          type: 'error',
+          text1: 'Error',
+          text2: 'Failed to upload image. Please try again.',
+          visibilityTime: 3000,
+        });
       }
     } catch (error) {
       console.error('Error in image picking/uploading:', error);
@@ -152,6 +164,12 @@ const UpdateProfileScreen = ({navigation, route}) => {
         ...prev,
         profilePicUrl: 'Error uploading image. Please try again.',
       }));
+       Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Failed to upload image. Please try again.',
+        visibilityTime: 3000,
+      });
     } finally {
       setIsUploading(false);
     }
